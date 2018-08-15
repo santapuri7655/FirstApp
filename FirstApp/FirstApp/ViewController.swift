83b40f9b72be17a10d3694143f48b7ca251b0e59
Radhika Santapuri <radhika.santapuri@gmail.com>
2018-08-15 08:20:00 -0500

    tip amount calculation

--- FirstApp/FirstApp/ViewController.swift
+++ FirstApp/FirstApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var totalTextField: UITextField? //UITestField!
+    @IBOutlet weak var tipTextField: UITextField?   //UITestField!
+    @IBOutlet weak var myFirstLabel: UILabel?       //UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        let total = totalTextField, //Double(totalTextField.text!)!
+        let tipPercentage = tipTextField/100.0 //Double(tipTextField.text!)!
+        let tip = total * tipPercentage
+        
+        if tip >= 50.0 {
+            myFirstLabel.text = String("Tip: $\(tip)‼️")
+        } else {
+            myFirstLabel.text = String("Tip: $\(tip)")
+        }
 
+    }
+    
 }
 
