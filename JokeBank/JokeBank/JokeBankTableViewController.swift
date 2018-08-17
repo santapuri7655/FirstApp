ed4064c8d07b3468c55a2795405e97e0d93de94b
Radhika Santapuri <radhika.santapuri@gmail.com>
2018-08-17 08:37:23 -0500

    segues, connecting segues, table views

--- JokeBank/JokeBank/JokeBankTableViewController.swift
+++ JokeBank/JokeBank/JokeBankTableViewController.swift
@@ -9,82 +9,59 @@
 import UIKit
 
 class JokeBankTableViewController: UITableViewController {
+    
+    struct JokeBank {
+        static let CleanJokes = "clean jokes"
+        static let RacistJokes = "racist jokes"
+        static let JokeOfTheDay = "joke of the day"
+        static let LatestJokes = "latest jokes"
+        static let FamilyJokes = "family jokes"
+    }
+    
+    var Jokes = [JokeBank.CleanJokes, JokeBank.RacistJokes, JokeBank.JokeOfTheDay, JokeBank.LatestJokes, JokeBank.FamilyJokes]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    //How many?
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Jokes.count //Returning number of things to view inside of the table
     }
 
-    /*
+    //What goes inside?
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = UITableViewCell()
+        
+        //print(indexPath.row) --> cronological sequence of number of items in an Array. Prints 0,1,2,3,4 --> number of elements in an array.
+        
+        cell.textLabel?.text = Jokes[indexPath.row]
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    //inside this func -> what happens when you tap on a cell in function
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+        print(indexPath.row)
+        
+        let selectedJoke = Jokes[indexPath.row]
+        
+        performSegue(withIdentifier: "moveToJokeDefinition" , sender: selectedJoke)
+        //withIdentifier: attributes inspector > indetifier > "identifier name"
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if let jokeVC = segue.destination as? JokeBankViewController {
+            
+            if let selectedJoke = sender as? String {
+                jokeVC.joke = selectedJoke
+            }
+            
+            
+        }
     }
-    */
 
 }
---
+++ JokeBank/JokeBank/JokeBankViewController.swift
@@ -0,0 +1,24 @@
+//
+//  JokeBankViewController.swift
+//  JokeBank
+//
+//  Created by radhika santapuri on 8/17/18.
+//  Copyright © 2018 radhika santapuri. All rights reserved.
+//
+
+import UIKit
+
+class JokeBankViewController: UIViewController {
+    
+    var joke = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = joke
+        
+    }
+    
+     
+    
+}
