fef601e89348d68851595b0b9a971d28292ae8f4
Radhika Santapuri <radhika.santapuri@gmail.com>
2018-08-22 07:02:52 -0500

    Joke bank full app

--- JokeBank/JokeBank/JokeBankViewController.swift
+++ JokeBank/JokeBank/JokeBankViewController.swift
@@ -13,7 +13,8 @@ class JokeBankViewController: UIViewController {
     struct JokeDefinition {
         static let cleanJokes = "A child asked his father, How were people born? So his father said, Adam and Eve made babies, then their babies became adults and made babies, and so on. The child then went to his mother, asked her the same question and she told him, We were monkeys then we evolved to become like we are now. The child ran back to his father and said, You lied to me! His father replied, No, your mom was talking about her side of the family."
         static let JokeOfTheDay = "No jokes found matching your search terms."
-        static let LatestJokes = ""
+        static let LatestJokes = "This is my latest joke"
+        static let FamilyJokes = "Family jokes are fun."
     }
     
         @IBOutlet weak var displayJoke: UITextView!
@@ -23,9 +24,18 @@ class JokeBankViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = joke
-    
-        if joke == JokeBankTableViewController.JokeBank.CleanJokes {
+        
+        switch joke {
+        case JokeBankTableViewController.JokeBank.CleanJokes:
             displayJoke.text = JokeDefinition.cleanJokes
+        case JokeBankTableViewController.JokeBank.FamilyJokes:
+            displayJoke.text = JokeDefinition.FamilyJokes
+        case JokeBankTableViewController.JokeBank.LatestJokes:
+            displayJoke.text = JokeDefinition.LatestJokes
+        case JokeBankTableViewController.JokeBank.JokeOfTheDay:
+            displayJoke.text = JokeDefinition.JokeOfTheDay
+        default:
+            print("None selected")
         }
         
     }
